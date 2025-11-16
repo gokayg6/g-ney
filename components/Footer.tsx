4b3f31d80bc859dee3f90b74cb5b0ff12eda05d5
Gokay <seningithubmailin@gmail.com>
2025-11-17 00:40:03 +0300

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,17 +8,6 @@ const Footer: React.FC<{}> = () => {
   const [socialIcons, setSocialIcons] = useState<SocialIcon[]>([]);
 
   useEffect(() => {
-    var Tawk_LoadStart = new Date();
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = process.env.NEXT_PUBLIC_TAWK_TO_ID ?? "";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode?.insertBefore(s1, s0);
-    })();
-
     // Load social icons from API
     fetch("/api/content")
       .then((res) => res.json())
